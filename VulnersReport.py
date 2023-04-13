628d9bd6a665a92d48ad9c2f7fbb17cb51f24f59
Julian Chavez <69224655+Jofsec@users.noreply.github.com>
2023-04-12 21:44:11 -0700

    Update VulnersReport.py
    Updated script

--- VulnersReport.py
+++ VulnersReport.py
@@ -1,51 +1,44 @@
 #!/usr/bin/env python3.8
 
-
-#import pyfiglet
-import sys
 import xml.dom.minidom
 import vulners
 import subprocess
 
-#text = pyfiglet.figlet_format("\nVulners Report", font="roman")
-#print(text)
+def print_vulnerability_info(cpe_info, vulnerability_info):
+    filtered_keys = ["id", "cvss", "href", "description"]
+    results = [vulnerability_info.get(key) for key in filtered_keys]
+    print(f"{cpe_info}:\n{'-'*100}\n"
+          f"{filtered_keys[0].upper()}: {results[0]}\n"
+          f"{filtered_keys[1].upper()}: {results[1]}\n"
+          f"Hypertext Reference: {results[2]}\n"
+          f"{filtered_keys[3].title()}: {results[3]}\n"
+          f"{'-'*100}\n")
 
-def parse_nmap_xml(xml_file_path):
-    vulners_api = vulners.Vulners(api_key="your_api_key")
+def get_vulnerabilities(cpe_info, vulners_api):
+    try:
+        results = vulners_api.cpeVulnerabilities(cpe_info)
+        vulnerability_info_list = [results.get(key) for key in results if key not in ["info", "blog", "bugbounty"]]
+        for vulnerability_info in vulnerability_info_list[0]:
+            print_vulnerability_info(cpe_info, vulnerability_info)
+    except ValueError:
+        print(f"Invalid CPE format {cpe_info}\n")
 
-    # create the XML object, usually known as a Tree
+def parse_nmap_xml(xml_file_path, vulners_api):
     dom_tree = xml.dom.minidom.parse(xml_file_path)
-    # access the first element of the Tree
     nmaprun = dom_tree.documentElement
-    # <cpe>cpe:/a:samba:samba</cpe>
     for element in nmaprun.getElementsByTagName("cpe"):
-        # print(element.childNodes[0].data)
         cpe_info = element.childNodes[0].data
-        # grab cpe string and call vulnerability with string
-        try:
-            cpe_results = vulners_api.cpeVulnerabilities(cpe_info)
-            cpe_vulnerabilities_list = [cpe_results.get(key) for key in cpe_results if
-                                        key not in ["info", "blog", "bugbounty"]]
-
-            for cpe_list in cpe_vulnerabilities_list[0]:
-                filtered_keys = ["id", "cvss", "href", "description"]
-                results = [cpe_list[key] for key in filtered_keys]
-                print(cpe_info + ":\n\n" + filtered_keys[0].upper() + ": " + str(results[0]) + "\n\n" + filtered_keys[1].upper() + ": " + str(results[1]) + "\n\n" + "Hypertext Reference: " + str(results[2]) + "\n\n" + filtered_keys[3].title() + ": " + str(results[3]))
-                print( "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------" + "\n")
-
-        except ValueError:
-            print("\nInvalid CPE format " + cpe_info +  "\n\n")
-            continue
-
+        get_vulnerabilities(cpe_info, vulners_api)
 
 def main():
+    api_key = "your_api_key"
+    vulners_api = vulners.Vulners(api_key)
     ip_address = input("ip_address > ")
 
-    subprocess.run(["nmap", "-sV", "-O", "-T4", "-p", "1-1000", "-oX", "nmap_output.xml", ip_address],
-                                  capture_output=True, text=True)
+    nmap_command = ["nmap", "-sV", "-O", "-T4", "-p", "1-1000", "-oX", "nmap_output.xml", ip_address]
+    subprocess.run(nmap_command, capture_output=True, text=True)
     xml_file_path = "nmap_output.xml"
-    parse_nmap_xml(xml_file_path)
-
+    parse_nmap_xml(xml_file_path, vulners_api)
 
 if __name__ == '__main__':
     main()
